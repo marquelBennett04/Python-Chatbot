65a9e0e033e503d96db8a24e1b147167ff9607fc
marquelBennett04 <marqulnb@gmail.com>
2022-12-22 15:00:06 -0500

    Created a base file but having problems getting a response without putting "quit", other than that it does what it's meant to do

--- Bot.py
+++ Bot.py
@@ -1,3 +1,21 @@
-From chatterbot 
-import Chatbot
+import sys
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+#Creating the new bot
 chatbot = ChatBot("My Bot")
+
+#Training the bot
+bot_trainer = ChatterBotCorpusTrainer(chatbot)
+bot_trainer.train(
+    "chatterbot.corpus.english"
+)
+
+#Testing the bot
+print("enter 'quit' to stop")
+while True:
+    user_input = input("You: ")
+    if user_input == 'quit':
+            break
+print("Bot:", chatbot.get_response(user_input))
